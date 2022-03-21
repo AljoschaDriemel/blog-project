2cf549371606867484eed739427c5d1e4c0217f6
ismail <iduzel49@gmail.com>
2022-03-21 14:59:34 +0100

    appbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import PrimarySearchAppBar from './components/footer/appbar/Appbar';
 
 function App() {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
      <Routes>
        <Route path='/' element={<Home />}/>
      </Routes>
