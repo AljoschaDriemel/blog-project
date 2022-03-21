72aac151bd9b262a7e88d12573ecce3e00bfcefe
ismail <iduzel49@gmail.com>
2022-03-21 15:53:31 +0100

    added some setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,27 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
-import PrimarySearchAppBar from './components/footer/appbar/Appbar';
+import PrimarySearchAppBar from './components/appbar/Appbar';
+import { Col, Row } from 'react-bootstrap'
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <PrimarySearchAppBar />
+      <Row>
+          <Col> 
+          <PrimarySearchAppBar />
+          </Col>
+      </Row>
+      <Row>
+        <Col className='col-3'>
+        <Navbar />
+        </Col>
+        <Col className='col-9'>MAIN</Col>
+      </Row>
+
+     
+
      <Routes>
        <Route path='/' element={<Home />}/>
      </Routes>
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.scss'
+
+export default function Navbar() {
+  return (
+    <div className='navbarcomp'>
+        <h2 >Dashboard</h2>
+        <h2 className='bg-danger'>Category</h2>
+        <h2>Post</h2>
+    </div>
+  )
+}
